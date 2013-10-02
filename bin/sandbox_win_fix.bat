831fc810913d6d7f477021cb2bc3a01fa42bafad
Xuan Choo <xchoo@hotmail.com>
2013-10-02 11:26:39 -0400

    Fix for sandbox.blend automation test commit.

--- bin/sandbox_win_fix.bat
+++ bin/sandbox_win_fix.bat
@@ -1,4 +1,4 @@
-echo "Run this from the morse root installation folder"
-cd share\morse\data\environments
+echo "Run this from the morse bin installation folder"
+cd ..\share\morse\data\environments
 del sandbox.blend
 mklink sandbox.blend "indoors-1\indoor-1.blend"
